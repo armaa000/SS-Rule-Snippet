42438efcdc578d88eb5a4f4d11a7de6b3926351c
Aaron <18231984+armaa000@users.noreply.github.com>
2021-10-25 19:00:47 +0800

    Update iOS_Weather_AQI_Standard.js

--- Scripts/Surge/iOS_Weather_AQI_Standard.js
+++ Scripts/Surge/iOS_Weather_AQI_Standard.js
@@ -13,8 +13,8 @@ const aqicnToken = 'd2e79a487e07acd2027c0e35b8dc5131185c92d4'
 */
 
 const AirQualityStandard = {
-	CN: 'HJ6332012.2111',
-	US: 'EPA_NowCast.2111'
+	CN: 'HJ6332012.2113',
+	US: 'EPA_NowCast.2113'
 }
 
 const AirQualityLevel = {
